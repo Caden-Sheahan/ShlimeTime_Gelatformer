2ae8980a66248a3115240fa0a658bf53d07ca576
rswansonbradley <raswanson@mail.bradley.edu>
2022-11-21 10:27:38 -0600

    SomeMinorStuff

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -18,11 +18,6 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            SceneManager.LoadScene(0);
-        }  
-        
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             PauseGame();
